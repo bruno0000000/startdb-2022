ec1cc65591cc130565290ca4b84f5fc9d72681f6
bruno0000000 <109878393+bruno0000000@users.noreply.github.com>
2022-07-23 20:51:57 -0300

    Alteração final no projeto
    Todas as regras validadas

--- desafio/forca.js
+++ desafio/forca.js
@@ -1,15 +1,95 @@
 class Forca {
 
-  chutar(letra) { }
+  constructor(palavraSecreta) {
+    this.palavraSecreta = palavraSecreta;
+    this.palavra = new Array(palavraSecreta.length + 1).join("_").split("");
+    this.letrasChutadas = [];
+    this.vidas = 6;
+    this.estado = "aguardando chute";
+  }
 
-  buscarEstado() { return ""; } // Possiveis valores: "perdeu", "aguardando chute" ou "ganhou"
+  getLetrasChutadas() {
+    return this.letrasChutadas;
+  }
+  getPalavraSecreta() {
+    return this.palavraSecreta;
+  }
+
+  getPalavra() {
+    return this.palavra;
+  }
+
+  getVidas() {
+    return this.vidas;
+  }
+
+  setVidas() {
+    return this.vidas -= 1;
+  }
+
+  getEstado() {
+    return this.estado;
+  }
+
+  setEstado(estado) {
+    return this.estado = estado;
+  }
+
+  ehLetra(letra) {
+    return letra.length == 1;
+  }
+
+  ehNumero(letra) {
+    return !isNaN(letra);
+  }
+
+  ehCaracterEspecial(letra) {
+    let regex = /\W|_/;
+    return regex.test(letra);
+  }
+  estaPresente(letra) {
+    return this.letrasChutadas.includes(letra);
+  }
+
+  chutar(letra) {
+    let letraChutada = this.getLetrasChutadas();
+    let palavraSecreta = this.getPalavraSecreta();
+    let palavra = this.getPalavra();
+    let vida = this.getVidas();
+
+    if (this.ehLetra(letra) && !this.estaPresente(letra) && !this.ehNumero(letra) && !this.ehCaracterEspecial(letra)) {
+      letraChutada.push(letra);
+
+      if (palavraSecreta.includes(letra)) {
+        for (let i = 0; i < palavraSecreta.length; i++) {
+          if (palavraSecreta[i] == letra) {
+            palavra[i] = letra;
+          }
+        }
+
+        if (!palavra.includes('_')) {
+          this.setEstado("ganhou");
+        }
+      } else {
+        this.setVidas();
+        vida = this.getVidas();
+        if (vida == 0) {
+          this.setEstado("perdeu");
+        }
+      }
+    }
+  }
+
+  buscarEstado() {
+    return this.getEstado();
+  }
 
   buscarDadosDoJogo() {
-      return {
-          letrasChutadas: [], // Deve conter todas as letras chutadas
-          vidas: 6, // Quantidade de vidas restantes
-          palavra: [] // Deve ser um array com as letras que já foram acertadas ou o valor "_" para as letras não identificadas
-      }
+    return {
+      letrasChutadas: this.getLetrasChutadas(),
+      vidas: this.getVidas(),
+      palavra: this.getPalavra()
+    }
   }
 }
 
